527c36bec9609d4b951acc97b310dbfd70ee532e
jfzo <juanzox@gmail.com>
2016-06-10 16:41:15 -0300

    pytho code for near neighbor computation added

--- matlab/DistributedCoresetAndPCA/example.m
+++ matlab/DistributedCoresetAndPCA/example.m
@@ -27,89 +27,150 @@ path('/Users/jz/git/distributed-clustering/matlab/DistributedCoresetAndPCA', pat
     gscatter(Z(:,1),Z(:,2),Z(:,3))
     Pwclass = Z;
     P = Z(:,1:2);
-    %%
-      
+%% Spiral
+clear;
+Pwclass=importdata('../data/spiral.csv');
+gscatter(Pwclass(:,1),Pwclass(:,2),Pwclass(:,3))
+% adding noise
+P = Pwclass(:,1:2);
+sigma_1 = 0.1;
+sigma_2 = 0.1;
+NOISE = mvnrnd([0,0], [sigma_1,0;0,sigma_2], round(size(P,1)));
+%P = vertcat(P, NOISE);
+P = P + NOISE;
+
+gscatter(P(:,1),P(:,2),Pwclass(:,3))
+%% Coreset routine    
+K = length(unique(Pwclass(:,3))); % it can be manually set also!    
+
+%'Random'graph generation with n=9, p=0.3
+Nnodes = 6;
+G= random_graph_gen(Nnodes, 0.3);    
+fprintf('generated random graph\n');
+
+%partitioning data into 9 local sets using 'weighted' partition method
+[N, dim] = size(P);
+indn=get_partition('weighted', N, Nnodes, sum(G), 1, P);
+
+%Distributed PCA of the data with t_vector = [14]
+%%proj_vector = distributed_pca(P, [14], 9, indn);
+%%lowDim_P = P*proj_vector{1};
+lowDim_P = P;
+
+global LOC_CORESET_CENTERS
+LOC_CORESET_CENTERS = zeros(Nnodes*K, 2);
+%Distributed_coreset construction and lloyd's k-means impementation
+%for the PCA data with k=10, t=10% of the size of the data
+[S,w] = distributed_coreset(lowDim_P, indn, Nnodes, K, floor(0.5*N) );
+[centers_coreset]=lloyd_kmeans(K, S, w);
+[centers_entire]=lloyd_kmeans(K, P);
     
-    K = length(unique(Pwclass(:,3))); % it can be manually set also!    
-    
-	%'Random'graph generation with n=9, p=0.3
-    Nnodes = 6;
-    G= random_graph_gen(Nnodes, 0.3);    
-    fprintf('generated random graph\n');
-   
-    %partitioning data into 9 local sets using 'weighted' partition method
-    [N, dim] = size(P);
-    indn=get_partition('weighted', N, Nnodes, sum(G), 1, P);
-    
-    %Distributed PCA of the data with t_vector = [14]
-    %%proj_vector = distributed_pca(P, [14], 9, indn);
-    %%lowDim_P = P*proj_vector{1};
-    lowDim_P = P;
-    
-    global LOC_CORESET_CENTERS
-    LOC_CORESET_CENTERS = zeros(Nnodes*K, 2);
-    %Distributed_coreset construction and lloyd's k-means impementation
-    %for the PCA data with k=10, t=10% of the size of the data
-    [S,w] = distributed_coreset(lowDim_P, indn, Nnodes, K, floor(0.1*N) );
-    [centers_coreset]=lloyd_kmeans(K, S, w);
-    [centers_entire]=lloyd_kmeans(K, P);
-    
-    %% Finding the closest center to each coreset point
-    dims = size(S);
-    labeledS = zeros(dims(1), dims(2) + 1);
-    labeledS(:,1:2) = S;
-    
-    for i=1:dims(1)
-        min_d = inf;
-        min_c = -1;
-        for c=1:size(centers_coreset,1)
-            curr_d = sum((S(i,:)-centers_coreset(c,:)).^2);
-            if curr_d < min_d
-                min_d = curr_d;
-                min_c = c;
-            end
+%% Using DBSCAN over original data
+path('/Users/jz/git/distributed-clustering/matlab/DBSCAN', path())
+epsilon=2;
+MinPts=2;
+IDX=DBSCAN(P,epsilon,MinPts);
+gscatter(P(:,1),P(:,2),IDX)
+
+%% Using DBSCAN over coreset data
+% Barrer los valores de parametros para los cuales la cantidad de Clusters
+% encontrados es igual a 3. MinPts esta en [0,] / epsilon esta en [0,]
+
+params_factibles = {};
+c = 0;
+for epsilon=0:10e-3:5
+    for MinPts=1:10
+        IDX=DBSCAN(S,epsilon,MinPts);
+        k_obtenido = length(unique(IDX));
+        if k_obtenido == 3
+            c = c + 1;
+            params_factibles{c} = [epsilon, MinPts];
         end
-        labeledS(i,3) = min_c;
     end
-    
+end
+
+params = {};
+c = 0;
+TH = 0.04;
+for i=1:length(params_factibles)
+    IDX=DBSCAN(S,params_factibles{1}(1),params_factibles{1}(2));
+    % solo 3 clases
+    if (sum(IDX==0)/length(IDX) > TH) && (sum(IDX==1)/length(IDX) > TH) && (sum(IDX==2)/length(IDX) > TH)
+        c = c + 1;
+        params{c} = [params_factibles{1}(1),params_factibles{1}(2)];
+    end
+end
+gscatter(S(:,1),S(:,2),IDX)
+%% Finding the closest center to each coreset point
+dims = size(S);
+labeledS = zeros(dims(1), dims(2) + 1);
+labeledS(:,1:2) = S;
+
+for i=1:dims(1)
+    min_d = inf;
+    min_c = -1;
+    for c=1:size(centers_coreset,1)
+        curr_d = sum((S(i,:)-centers_coreset(c,:)).^2);
+        if curr_d < min_d
+            min_d = curr_d;
+            min_c = c;
+        end
+    end
+    labeledS(i,3) = min_c;
+end
+
+figure1 = figure('PaperSize',[20.98404194812 29.67743169791]);
+axes1 = axes('Parent',figure1);
+hold(axes1,'all');
+
+line(local_centers(:,1),local_centers(:,2),'Parent',axes1,'MarkerFaceColor',[0 1 0],'Marker','diamond','LineStyle','none',...
+'Color',[1 0.5 0.5],'MarkerSize',10,'DisplayName','Local node centers');
+
+gscatter(P(:,1),P(:,2),ones(size(P,1)),'m')
+gscatter(labeledS(:,1),labeledS(:,2),labeledS(:,3))
+
 %% Visualización
-    local_centers=zeros(Nnodes*K, 2);
-    j=1;
-    for i=1:Nnodes
-        local_centers(j,:) = LOC_CORESET_CENTERS{i}(1,:);
-        j=j+1;
-        local_centers(j,:) = LOC_CORESET_CENTERS{i}(2,:);
-        j=j+1;
-    end
-    
-    figure1 = figure('PaperSize',[20.98404194812 29.67743169791]);
-    axes1 = axes('Parent',figure1);
-    hold(axes1,'all');
+local_centers=zeros(Nnodes*K, 2);
+j=1;
+for i=1:Nnodes
+    local_centers(j,:) = LOC_CORESET_CENTERS{i}(1,:);
+    j=j+1;
+    local_centers(j,:) = LOC_CORESET_CENTERS{i}(2,:);
+    j=j+1;
+end
 
-    line(local_centers(:,1),local_centers(:,2),'Parent',axes1,'MarkerFaceColor',[0 1 0],'Marker','diamond','LineStyle','none',...
-    'Color',[1 0.5 0.5],'MarkerSize',10,'DisplayName','Local node centers');
+figure1 = figure('PaperSize',[20.98404194812 29.67743169791]);
+axes1 = axes('Parent',figure1);
+hold(axes1,'all');
 
-    
-    plot(S(:,1), S(:,2),'Parent',axes1,'MarkerFaceColor',...
-    [0.749019622802734 0.749019622802734 0],'MarkerSize',8,'Marker','+','LineStyle','none',...
-    'DisplayName','Coreset data');
+line(local_centers(:,1),local_centers(:,2),'Parent',axes1,'MarkerFaceColor',[0 1 0],'Marker','diamond','LineStyle','none',...
+'Color',[1 0.5 0.5],'MarkerSize',10,'DisplayName','Local node centers');
 
-    plot(centers_coreset(:,1), centers_coreset(:,2),'Parent',axes1,'MarkerSize',10,'Marker',...
-    'x','LineWidth',4,'LineStyle','none','Color',[0.87058824300766 0.490196079015732 0],...
-    'DisplayName','Coreset centroids');
 
-    plot(centers_entire(:,1), centers_entire(:,2),'Parent',axes1,'MarkerSize',8,'Marker',...
-    '*','LineWidth',4,'LineStyle','none','Color',[0 0 0],...
-    'DisplayName','Real centroids');
-    
-    %gscatter(Z(:,1),Z(:,2),Z(:,3));
-    
-    line(Pwclass(Pwclass(:,3)==1,1),Pwclass(Pwclass(:,3)==1,2),'Parent',axes1,'Marker','square','LineStyle','none',...
-    'Color',[1 0 0],'DisplayName','Orig.Data, class +');
+plot(S(:,1), S(:,2),'Parent',axes1,'MarkerFaceColor',...
+[0.749019622802734 0.749019622802734 0],'MarkerSize',8,'Marker','+','LineStyle','none',...
+'DisplayName','Coreset data');
 
-    line(Pwclass(Pwclass(:,3)==3,1),Pwclass(Pwclass(:,3)==3,2),'Parent',axes1,'MarkerSize',8,'Marker','v',...
-    'LineStyle','none','Color',[0 1 1],'DisplayName','Orig.Data, class -');
-    legend(axes1,'show');
+plot(centers_coreset(:,1), centers_coreset(:,2),'Parent',axes1,'MarkerSize',10,'Marker',...
+'x','LineWidth',4,'LineStyle','none','Color',[0.87058824300766 0.490196079015732 0],...
+'DisplayName','Coreset centroids');
+
+plot(centers_entire(:,1), centers_entire(:,2),'Parent',axes1,'MarkerSize',8,'Marker',...
+'*','LineWidth',4,'LineStyle','none','Color',[0 0 0],...
+'DisplayName','Real centroids');
+
+%gscatter(Z(:,1),Z(:,2),Z(:,3));
+
+line(P(Pwclass(:,3)==1,1),P(Pwclass(:,3)==1,2),'Parent',axes1,'Marker','square','LineStyle','none',...
+'Color',[1 0 0],'DisplayName','Orig.Data, class +');
+
+line(P(Pwclass(:,3)==2,1),P(Pwclass(:,3)==2,2),'Parent',axes1,'MarkerSize',8,'Marker','v',...
+'LineStyle','none','Color',[0 1 1],'DisplayName','Orig.Data, class -');
+legend(axes1,'show');
+
+line(P(Pwclass(:,3)==3,1),P(Pwclass(:,3)==3,2),'Parent',axes1,'MarkerSize',8,'Marker','o',...
+'LineStyle','none','Color',[0 0 1],'DisplayName','Orig.Data, class -');
+legend(axes1,'show');
 %%
     
     centers_dim = centers_coreset*proj_vector{1}';
---
+++ python/near_neighbor_similarity.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+
+'''
+IndexJoin technique.
+
+Input file must be in Cluto's format. That is, the 1st line contains 3 fields, namely
+the number of instances, the dimensionality of the vector space and the number of 
+non zero values.
+After that, every line contains a ' ' separated list of values, one for the column index 
+(starting from 1) and another for the column value.
+'''
+
+
+class InstanceFeature(object):
+    '''
+    This class is used to override the comparison operators of a two value tuple.
+    In this way, the heapq can be used as a max queue and not only as a min queue.
+    '''
+    def __init__(self, f_idx, f_val):
+        self.f_idx = f_idx
+        self.f_val = f_val
+        
+    def __eq__(self, other):
+        if isinstance(other, InstanceFeature):
+            return self.f_val == other.f_val
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, InstanceFeature):
+            return self.f_val > other.f_val # inverted in order to allow a max-heap
+        return NotImplemented
+        
+    def __gt__(self, other):
+        if isinstance(other, InstanceFeature):
+            return self.f_val < other.f_val # inverted in order to allow a max-heap
+        return NotImplemented
+    
+    def __str__(self):
+        return "IX:{0} -> VAL:{1:.4f}\n".format(self.f_idx, self.f_val)
+
+
+
+
+import sys
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-k","--nnbrs", type=int, help="Number of neighbors to consider")
+parser.add_argument("-o","--outfile", type=str, help="Path to the output file where the net will be stored")
+parser.add_argument("-i","--inputfile", type=str, help="Path to the input file in Cluto's format where the sparse vectors are stored")
+args = parser.parse_args()
+
+if not args.nnbrs or not args.outfile:
+    print "Usage: ./near_neighbor_similarity -k <near neighbor value> -i <path to the input file> -o <path to output file>"
+    sys.exit
+
+k = args.nnbrs
+outputfile = args.outfile
+inputfile = args.inputfile
+
+print "Reading", inputfile,"..."
+in_fm = open(inputfile)
+N,D,_ = map(int, in_fm.readline().strip().split())
+
+idx = range(D)
+for i in range(D):
+    idx[i] = {} # empty dict
+
+doc_ix = 1
+for L in in_fm:
+    data_L = L.strip().split()
+    for i in range(0,len(data_L),2):
+        f_ix = int(data_L[i])
+        f_val = float(data_L[i+1])
+        idx[f_ix - 1][doc_ix] = f_val
+    doc_ix += 1
+
+
+
+in_fm.seek(0) # reversing file manager
+in_fm.readline() # bypassing header
+import numpy as np
+
+S = np.zeros((N,N))
+doc_ix = 1
+for L in in_fm:
+    data_L = L.strip().split()
+    for i in range(0,len(data_L),2):
+        f_ix = int(data_L[i])
+        # check that feature in the index
+        for Dc in idx[f_ix - 1]:
+            if Dc > doc_ix: # symmetric matrix, hence only upper diagonal values matter.
+                S[doc_ix - 1, Dc - 1] += idx[f_ix - 1][doc_ix] * idx[f_ix - 1][Dc]
+            
+    doc_ix += 1
+in_fm.close()
+
+'''
+Filters the similarity matrix by selecting only the highest k values of similarity 
+in each row.
+'''
+
+
+from heapq import *
+
+print "Filtering near neighbors (k=",k,")"
+
+for i in range(N):
+    new_s_i = np.zeros(N)
+    h = []
+    candidates = np.where( S[0,:] > 0 )[0]
+    for c in candidates:
+        heappush(h, InstanceFeature(c,S[i,c]) )
+    nnbrs = [heappop(h) for n in range(k)] # list of InstanceFeature objs.
+    for n in nnbrs:
+        new_s_i[n.f_idx] = n.f_val
+    S[i,:] = new_s_i
+
+print "Writing adjacency matrix to",outputfile
+f = open(outputfile,"w")
+S.tofile(f, sep=" ", format="%.4f")
+f.close()
