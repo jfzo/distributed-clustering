03bcca39bb7f76fd6e5d82c8254db0c600e4af50
Héctor Allende-Cid <hallende@gmail.com>
2016-06-15 12:27:06 -0400

    Update

--- matlab/Spectral/demo.m
+++ matlab/Spectral/demo.m
@@ -1,21 +1,21 @@
 path('../DistributedCoresetAndPCA', path())
 %%
 clear;
-P=importdata('../data/vary-density.csv');
+P=importdata('../data/spiral.csv');
 Pwclass = P;
-Z = zeros(size(Pwclass));
-Z(1:50,:) = Pwclass(Pwclass(:,3)==3,:);
-Z(51:100,1) = Pwclass(Pwclass(:,3)==2,1)-0.2;
-Z(51:100,2) = Pwclass(Pwclass(:,3)==2,2)-0.2;
-Z(51:100,3) = zeros(50,1)+1;
-Z(101:end,:) = Pwclass(Pwclass(:,3)==1,:);
-gscatter(Z(:,1),Z(:,2),Z(:,3))
-Pwclass = Z;
-P = Z(:,1:2);
+P=P(:,1:2);
+
+sigma_1 = 0.1;
+sigma_2 = 0.1;
+NOISE = mvnrnd([0,0], [sigma_1,0;0,sigma_2], round(size(P,1)));
+%P = vertcat(P, NOISE);
+P = P + NOISE;
+
+gscatter(P(:,1),P(:,2))
 %%
 K = length(unique(Pwclass(:,3))); % it can be manually set also!    
 A = pdist2(P,P);
-sigma = 1;
+sigma = 0.95;
 A = exp(-(A.^2)/(2*sigma^2));
 for i=1:size(A,1)   
     A(i,i ) = 0;
@@ -46,22 +46,22 @@ for i=1:dims(1)
 end
 
 %%
-gscatter(P(:,1),P(:,2),ones(size(P,1)),'m')
-gscatter(Y(:,1),Y(:,2),labeledS(:,1))
+%gscatter(P(:,1),P(:,2),ones(size(P,1)),'m')
+%gscatter(Y(:,1),Y(:,2),labeledS(:,1))
 
-figure1 = figure('PaperSize',[20.98404194812 29.67743169791]);
-axes1 = axes('Parent',figure1);
-hold(axes1,'all');
+%figure1 = figure('PaperSize',[20.98404194812 29.67743169791]);
+%axes1 = axes('Parent',figure1);
+%hold(axes1,'all');
 
-line(P(Pwclass(:,3)==1,1),P(Pwclass(:,3)==1,2),'Parent',axes1,'Marker','square','LineStyle','none',...
-'Color',[1 0 0],'DisplayName','Orig.Data, class +');
+%line(P(Pwclass(:,3)==1,1),P(Pwclass(:,3)==1,2),'Parent',axes1,'Marker','square','LineStyle','none',...
+%'Color',[1 0 0],'DisplayName','Orig.Data, class +');
 
-line(P(Pwclass(:,3)==2,1),P(Pwclass(:,3)==2,2),'Parent',axes1,'MarkerSize',8,'Marker','v',...
-'LineStyle','none','Color',[0 1 1],'DisplayName','Orig.Data, class -');
-legend(axes1,'show');
+%line(P(Pwclass(:,3)==2,1),P(Pwclass(:,3)==2,2),'Parent',axes1,'MarkerSize',8,'Marker','v',...
+%'LineStyle','none','Color',[0 1 1],'DisplayName','Orig.Data, class -');
+%legend(axes1,'show');
 
-line(P(Pwclass(:,3)==3,1),P(Pwclass(:,3)==3,2),'Parent',axes1,'MarkerSize',8,'Marker','o',...
-'LineStyle','none','Color',[0 0 1],'DisplayName','Orig.Data, class -');
-legend(axes1,'show');
+%line(P(Pwclass(:,3)==3,1),P(Pwclass(:,3)==3,2),'Parent',axes1,'MarkerSize',8,'Marker','o',...
+%'LineStyle','none','Color',[0 0 1],'DisplayName','Orig.Data, class -');
+%legend(axes1,'show');
 
 gscatter(P(:,1),P(:,2),labeledS(:,1))
