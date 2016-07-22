01a2a6e1a5e22985b4a071d81bd63187df3a4678
JZ <juanzox@gmail.com>
2016-07-22 21:16:16 +0000

    packed snn clustering function v1

--- matlab/NNCoresetClustering/Distributed_SNN.m
+++ matlab/NNCoresetClustering/Distributed_SNN.m
@@ -1,38 +1,23 @@
-% Z was generated as the CURE dataset and labels are its group tags
-P = Z;
-Pwclass = horzcat(Z, labels);
+function [ CORE_PTS_CT, CORE_CLST_CT, CT_DATA, CT_DATA_LBLS ] = Distributed_SNN(DATASET, indn, Nnodes, SNN, K, Eps, MinPts, PCT_SAMPLE )
+%DISTRIBUTED_SNN Summary of this function goes here
+% Returns the resulting clusters of the transmitted core points from all
+% sources to a centralized node.
+% CORE_PTS_CT (vector): -1 if it is a noise point, 0 for non-core pts and 1 for core
+% pts.
+% CORE_CLST_CT (vector): 0 for noise and a value > 0 that denotes the
+% cluster label identified in the centralized node for the point.
+% CT_DATA (matrix): core-points transmitted from all nodes.
+% CT_DATA_LBLS: Original labels (ground truth) for the core-points transmitted from all nodes.
 
 
-K = length(unique(Pwclass(:,3))); % it can be manually set also!    
-
-%'Random'graph generation with n=9, p=0.3
-Nnodes = 4;
-G= random_graph_gen(Nnodes, 0.3);    
-fprintf('generated random graph\n');
-
-%partitioning data into 9 local sets using 'weighted' partition method
-[N, dim] = size(P);
-indn=get_partition('uniform', N, Nnodes, sum(G), 1, P);
-
-%% computing top-k near neighbors and shared near neighbors
-% Consider node 's'. Then SNN{s}{i} is an array that contains for point i, 
-% the number of snn's between item i and those items having an ID > i . 
-% THUS, SNN{s}{i}(1) has the value of snn's between i and (i+1) .
-
-K = 50;
-KNN = cell(Nnodes, 1);
-SNN = cell(Nnodes, 1);
-parfor s=1:Nnodes
-    localdata = P(indn==s,:);
-    [KNN{s}, SNN{s}] = compute_knn_snn(localdata, K);
-end
-
+DATA_LBLS = DATASET(:,end);
+DATA = DATASET(:,1:end-1); 
 %% Counting close points (in terms of SNN similarity) for each point ~ Density
-Eps = 34;
+%Eps = 34;
 
-Nnodes = 4;
+%Nnodes = 4;
 DST = cell(Nnodes, 1);
-parfor s=1:Nnodes 
+parfor s=1:Nnodes
     DST{s} = zeros(length(SNN{s}) + 1, 1); %array to store density
     for i=1:length(SNN{s})
         dense_ng = find(SNN{s}{i} > Eps);
@@ -44,7 +29,7 @@ parfor s=1:Nnodes
 end
 
 %% computing CORE points
-MinPts = 20;
+%MinPts = 20;
 % snn similarity function: e.g. snn_sim(SNN{1},7,1)
 snn_sim = @(SNN_info, i, j) SNN_info{min(i,j)}(abs(i-j));
 
@@ -55,7 +40,7 @@ parfor s=1:Nnodes
     CORE_PTS{s} = DST{s} > MinPts;
     CORE_CLST{s} = zeros(length(DST{s}),1);
     core_points = find(DST{s} > MinPts);
-  
+    
     clst_id = 0;
     for i=1:size(core_points, 1)-1 %identifying clusters
         if CORE_CLST{s}(core_points(i)) == 0
@@ -71,7 +56,7 @@ parfor s=1:Nnodes
 end
 % remember to filter the near ones.
 %% sampling core-points from each node
-PCT_SAMPLE = 0.3;
+%PCT_SAMPLE = 0.3;
 SAMPLED_CORE_PTS = cell(Nnodes, 1);
 parfor s=1:Nnodes
     %find(CORE_PTS{s} == 1)
@@ -82,7 +67,7 @@ parfor s=1:Nnodes
     for c=1:length(clst_s)
         if clst_s(c) == 0
             continue
-        end 
+        end
         %display(sprintf('[%d] Valor de c=%d \n', s, clst_s(c)));
         N_c = length(find(CORE_CLST{s} == clst_s(c)));
         %display(sprintf('Valor de N_c=%d \n', N_c));
@@ -93,8 +78,8 @@ parfor s=1:Nnodes
     SAMPLED_CORE_PTS{s} = randsample(length(CORE_CLST{s}), round(PCT_SAMPLE*N_s) ,true, PTS_s_W);
 end
 
-%% Centralizing the transmitted core-points into a single dataset 
-P = Pwclass(:,1:2);
+%% Centralizing the transmitted core-points into a single dataset
+%P = Pwclass(:,1:2);
 %M_1 = P(indn==1,:);
 % Perform SNN-clustering over the sampled core points.
 N_sampled = 0;
@@ -103,19 +88,26 @@ for s=1:Nnodes
 end
 
 CT_DATA = zeros(N_sampled, 2);
+CT_DATA_LBLS = zeros(N_sampled, 1);
 
-localdata = P(indn==1,:);
+localdata = DATA(indn==1,:);
+localdata_lbls = DATA_LBLS(indn==1,:);
+
 CT_DATA(1:length(SAMPLED_CORE_PTS{1}),:) = localdata(SAMPLED_CORE_PTS{1},:);
+CT_DATA_LBLS(1:length(SAMPLED_CORE_PTS{1}),:) = localdata_lbls(SAMPLED_CORE_PTS{1},:);
+
 offset = length(SAMPLED_CORE_PTS{1}) + 1;
-for s=2:Nnodes    
-    localdata = P(indn==s,:);
+for s=2:Nnodes
+    localdata = DATA(indn==s,:);
+    localdata_lbls = DATA_LBLS(indn==s,:);
     CT_DATA(offset:offset+length(SAMPLED_CORE_PTS{s})-1,:) = localdata(SAMPLED_CORE_PTS{s},:);
-    offset = offset + length(SAMPLED_CORE_PTS{s});    
+    CT_DATA_LBLS(offset:offset+length(SAMPLED_CORE_PTS{s})-1,:) = localdata_lbls(SAMPLED_CORE_PTS{s},:);
+    offset = offset + length(SAMPLED_CORE_PTS{s});
 end
 
 %% Apply SNN-clustering over CT_DATA
 
-K = 50;
+%K = 50;
 [KNN_CT, SNN_CT] = compute_knn_snn(CT_DATA, K);
 
 % Counting close points (in terms of SNN similarity) for each point ~ Density
@@ -130,7 +122,7 @@ for i=1:length(SNN_CT)
 end
 
 % Identifying CORE points
-MinPts = 30;
+%MinPts = 30;
 % snn similarity function: e.g. snn_sim(SNN{1},7,1)
 snn_sim = @(SNN_info, i, j) SNN_info{min(i,j)}(abs(i-j));
 CORE_PTS_CT = DST_CT > MinPts;
@@ -164,7 +156,7 @@ for i=1:length(non_core)
             nearest_core = core(j);
         end
     end
-    % if the snn similarity is lower than Eps => discard the point    
+    % if the snn similarity is lower than Eps => discard the point
     if nearest_sim < Eps
         CORE_PTS_CT(non_core(i)) = -1;
     else % otherwise, label the point with the nearest core point label.
@@ -172,81 +164,6 @@ for i=1:length(non_core)
     end
 end
 
-
-% Plotting core points with their identified labels
-scatter(CT_DATA(CORE_PTS_CT,1), CT_DATA(CORE_PTS_CT,2), 5, CORE_CLST_CT(CORE_PTS_CT),'o')
-% Plotting non-core/non-noise points with their identified labels
-non_core = find(CORE_PTS_CT ~= 0 );
-if isempty(non_core) > 0
-    scatter(CT_DATA(non_core,1), CT_DATA(non_core,2), 5, CORE_CLST_CT(non_core),'o')
+% end of all
 end
 
-% Export core and non-core/non-noise points with their labels 
-
-%[A,B] = SNNClustering( CT_DATA, K, Eps, MinPts);
-%% Testing the packed version of the code above!
-K = 50;
-Nnodes = 4;
-Eps = 34;
-MinPts = 20;
-PCT_SAMPLE = 0.3;
-
-G= random_graph_gen(Nnodes, 0.3);    
-[N, ~] = size(DATASET);
-indn = get_partition('uniform', N, Nnodes, sum(G), 1, Pwclass(:,1:2));
-
-KNN = cell(Nnodes, 1);
-SNN = cell(Nnodes, 1);
-parfor s=1:Nnodes
-    localdata = P(indn==s,:);
-    [KNN{s}, SNN{s}] = compute_knn_snn(localdata, K);
-end
-
-[ CORE_PTS_CT, CORE_CLST_CT, CT_DATA, CT_DATA_LBLS ] = Distributed_SNN(Pwclass, indn, Nnodes, SNN, K, Eps, MinPts, PCT_SAMPLE );
-%%
-figure
-subplot(2,1,1)
-scatter(CT_DATA(:,1), CT_DATA(:,2), 5, CT_DATA_LBLS,'o')
-title({['Centralized core-pts with original labels']});
-legend('off')
-subplot(2,1,2)
-scatter(CT_DATA(:,1), CT_DATA(:,2), 5, CORE_CLST_CT,'o')
-title({['Centralized core-pts with identified labels']});
-legend('off')
-
-%% Plotting for 4 nodes
-
-figure
-for s=1:Nnodes
-    subplot(2,2,s)
-    Dn = Pwclass(indn == s,:);hold on;
-    scatter(Dn(:,1),Dn(:,2), 2, '.k');
-    % %gscatter(Dn(CORE_PTS{s},1), Dn(CORE_PTS{s},2), CORE_CLST{s}(CORE_PTS{s}))
-    scatter(Dn(CORE_PTS{s},1), Dn(CORE_PTS{s},2), 5, CORE_CLST{s}(CORE_PTS{s}),'o')
-    title({['Core-pts in Node ',num2str(s)];[' (',num2str(100*sum(CORE_PTS{s})/size(Dn,1)),'%)']});
-    legend('off')
-end
-
-
-
-figure
-for s=1:Nnodes
-    subplot(2,2,s)
-    N = length(KNN{s});
-    knnG_s = zeros(length(KNN{s}));
-    for j=1:N-1
-        for nn=KNN{s}{j}
-            knnG_s(j,nn)= 1;
-            knnG_s(nn,j)= 1;
-        end
-    end
-
-    Gs=graph(knnG_s,'upper');
-    p = plot(Gs);
-    p.NodeColor = 'red';
-    p.LineWidth = 0.1;
-    Gs.Nodes.NodeColors = degree(Gs);
-    p.NodeCData = Gs.Nodes.NodeColors;
-    colorbar
-    title({['Node ',num2str(s)];['']});
-end
