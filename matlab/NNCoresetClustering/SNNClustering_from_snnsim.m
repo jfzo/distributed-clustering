948d1e6ac88ed2823a05e0dd6698e551a05575ef
JZ <juanzox@gmail.com>
2016-07-22 15:31:44 +0000

    update

--- matlab/NNCoresetClustering/SNNClustering_from_snnsim.m
+++ matlab/NNCoresetClustering/SNNClustering_from_snnsim.m
@@ -1,7 +1,9 @@
-function [CORE_PTS_CT, CORE_CLST_CT] = SNNClustering(DATA, K, Eps, MinPts)
-    % [CORE_PTS_CT, CORE_CLST_CT] = SNNClustering(DATA, Knn, Eps, MinPts)
+function [CORE_PTS_CT, CORE_CLST_CT] =  SNNClustering_from_snnsim(SNN_CT, Eps, MinPts)
+    % [CORE_PTS_CT, CORE_CLST_CT] = SNNClustering(SNN, Eps, MinPts)
     %
-    % Returns two vectors having the same length as rows the DATA matrix
+    % Receives the shared-nearest-neighbors similarity matrix together with
+    % Eps and MinPts parameters.
+    % Returns two vectors having the same length as rows the SNN matrix
     % has. 
     % CORE_PTS_CT contains in the i-th position:
     %   -1 , if point i is a noise point
@@ -11,9 +13,6 @@ function [CORE_PTS_CT, CORE_CLST_CT] = SNNClustering(DATA, K, Eps, MinPts)
     % CORE_CLST_CT contains a 0 for noise points and a value >0 denoting
     % the cluster label of non-core and core-points.
     tic
-    [KNN_CT, SNN_CT] = compute_knn_snn(DATA, K);
-
-    display('Sparsification complete. Starting SNN density computation...')
     
     % Counting close points (in terms of SNN similarity) for each point ~ Density
     DST_CT = zeros(length(SNN_CT) + 1, 1); %array to store density
@@ -82,86 +81,3 @@ function [CORE_PTS_CT, CORE_CLST_CT] = SNNClustering(DATA, K, Eps, MinPts)
 
     % Export core and non-core/non-noise points with their labels
 end
-
-% % parameters
-% % Eps y knn
-% Eps = 20;
-% knn = 20;
-% %%%%
-% clear;
-% clc;
-% Pwclass=importdata('noisy_circles.csv'); % Use parameter $\sigma=0.1$
-% gscatter(Pwclass(:,1),Pwclass(:,2),Pwclass(:,3))
-% % adding noise
-% P = Pwclass(:,1:2);
-% %% Building and sparsifying the distance matrix
-% A = pdist2(P,P);
-% 
-% for row=1:size(A,1)
-%     [Sval, Sind] = sort(A(row,:),'ascend');
-%     A(row, Sind(knn+2:end)) = 0; % consider self-similarity
-% end
-% %% SNN graph
-% B = zeros(size(A));
-% for row_i=1:(size(A,1)-1)
-%     for row_j=(row_i+1):size(A,1)
-%         B(row_i, row_j) = sum((A(row_i,:) > 0) & (A(row_j,:) > 0));
-%     end
-% end
-% display('Ok!');
-% 
-% %% CURE DATASET GENERATOR
-% clc;
-% % The big circle
-% mu = [0 -0.5];
-% Sigma = [5 0; 0 1]; R = chol(Sigma);
-% n1 = 3000;
-% z1 = repmat(mu,n1,1) + randn(n1,2)*R;
-% l1 = zeros(n1,1) + 1;
-% 
-% %the 1st elipse and its bridge
-% mu = [-2 6];
-% Sigma = [3 0; 0 0.05]; R = chol(Sigma);
-% n2 = 1000;
-% z2 = repmat(mu,n2,1) + randn(n2,2)*R;
-% l2 = zeros(n2,1) + 2;
-% 
-% 
-% mu = [1.0 5.9];
-% Sigma = [20 0; 0 0.01]; R = chol(Sigma);
-% n6 = 100;
-% z6 = repmat(mu,n6,1) + rand(n6,2)*R;
-% l6 = zeros(n6,1) + 2;
-% 
-% %the 2nd elipse
-% mu = [10 6];
-% Sigma = [3 0; 0 0.05]; R = chol(Sigma);
-% n3 = 1000;
-% z3 = repmat(mu,n3,1) + randn(n3,2)*R;
-% l3 = zeros(n3,1) + 3;
-% 
-% %the upper small circle
-% mu = [15 1];
-% Sigma = [0.3 0; 0 0.05]; R = chol(Sigma);
-% n4 = 1000;
-% z4 = repmat(mu,n4,1) + randn(n4,2)*R;
-% l4 = zeros(n4,1) + 4;
-% 
-% %the lower small circle
-% mu = [15 -2];
-% Sigma = [0.3 0; 0 0.05]; R = chol(Sigma);
-% n5 = 1000;
-% z5 = repmat(mu,n5,1) + randn(n5,2)*R;
-% l5 = zeros(n5,1) + 5;
-% 
-% % background noise
-% mu = [-8 -5];
-% Sigma = [700 0; 0 150]; R = chol(Sigma);
-% n7 = 1000;
-% z7 = repmat(mu,n7,1) + rand(n7,2)*R;
-% l7 = zeros(n7,1) + 7;
-% 
-% % merging 
-% Z = vertcat(z1, z2, z3, z4, z5,z6, z7);
-% labels = vertcat(l1,l2,l3,l4,l5,l6,l7);
-% gscatter(Z(:,1),Z(:,2), labels )
---
+++ matlab/NNCoresetClustering/snnclustering_tunning.m
@@ -0,0 +1,23 @@
+%%
+K = 50; % fixed
+DATA = Pwclass(:,1:2);
+[~, SNN_K] = compute_knn_snn(DATA, K);
+
+range_Eps = 15:5:40;
+range_MinPts = 15:5:30;
+
+results_K = cell(length(range_Eps),length(range_MinPts));
+
+for i=1:length(range_Eps)
+    for j=1:length(range_MinPts)
+        tic
+        Eps = range_Eps(i);
+        MinPts = range_MinPts(j);
+        
+        results_K{i,j} =  cell(2,1);
+       
+        display(sprintf('SNN-clustering with parameters Eps:%d MinPts:%d (K:%d)\n',Eps, MinPts, K));
+        [results_K{i,j}{1}, results_K{i,j}{2}] =  SNNClustering_from_snnsim(SNN_K, Eps, MinPts);
+        toc
+    end
+end
