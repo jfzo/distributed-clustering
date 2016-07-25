11319ff65655f51639a0ee22ec392289b80bf901
Juan Zamora O <juanzox@gmail.com>
2016-07-25 16:22:48 -0400

    script for plotting results

--- matlab/NNCoresetClustering/plot_results.m
+++ matlab/NNCoresetClustering/plot_results.m
@@ -1,7 +1,8 @@
 % ARI 4
 % AMI 5
 % VMeasure 6
-loaded_results = centralizadoK50;
+
+loaded_results = csvread('results/centralizado_K50.csv');;
 A = zeros(length(unique(loaded_results(:,2))),length(unique(loaded_results(:,3))) );
 h = 1;
 for i=1:length(unique(loaded_results(:,2)))
