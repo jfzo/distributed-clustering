accc2344af9f448fe7d95f51be85103541b1125e
JZ <juanzox@gmail.com>
2016-07-25 20:31:31 +0000

    figures added

--- matlab/NNCoresetClustering/plot_results.m
+++ matlab/NNCoresetClustering/plot_results.m
@@ -2,11 +2,18 @@
 % AMI 5
 % VMeasure 6
 
-loaded_results = csvread('results/centralizado_K50.csv');;
-A = zeros(length(unique(loaded_results(:,2))),length(unique(loaded_results(:,3))) );
-h = 1;
-for i=1:length(unique(loaded_results(:,2)))
-    arr = unique(loaded_results(:,2));
-    A(h,:) = loaded_results((loaded_results(:,2)==arr(i)), 4);
-    h = h + 1;
+for K=[50, 70, 90]
+    loaded_results = csvread(sprintf('results/distribuido_K%d.csv',K));
+    A = zeros(length(unique(loaded_results(:,2))),length(unique(loaded_results(:,3))) );
+    h = 1;
+    for i=1:length(unique(loaded_results(:,2)))
+        arr = unique(loaded_results(:,2));
+        A(h,:) = loaded_results((loaded_results(:,2)==arr(i)), 4);
+        h = h + 1;
+    end
+
+    h = tabularHeatMap(A);
+    h.XTickLabel =  unique(loaded_results(:,2));
+    h.YTickLabel = unique(loaded_results(:,3));
+    saveas(h, sprintf('results/distribuido_ARI_K%d.png',K));
 end
