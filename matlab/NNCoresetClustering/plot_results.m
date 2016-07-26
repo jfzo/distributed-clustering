1f7240794183eedfaeed173896cb25384adbb826
jfzo <juanzox@gmail.com>
2016-07-26 16:40:51 -0400

    some plots updated

--- matlab/NNCoresetClustering/plot_results.m
+++ matlab/NNCoresetClustering/plot_results.m
@@ -11,19 +11,19 @@ for K=[50, 70, 90]
     A = zeros(length(eps_values),length(minpts_values) );
     h = 1;
     for i=1:length(eps_values)
-        A(h,:) = loaded_results((loaded_results(:,2)==eps_values(i)), 4);
+        A(h,:) = loaded_results((loaded_results(:,2)==eps_values(i)), 6);
         h = h + 1;
     end
 
     h = tabularHeatMap(A);
     xlabel('MinPts');
     ylabel('Eps');
-    %h.YTick = transpose(eps_values);
+    h.YTick =  1:length(eps_values);
     h.YTickLabel =  num2cell(transpose(eps_values));
     h.XTick =  1:length(minpts_values);
     h.XTickLabel = num2cell(transpose(minpts_values));
 
-    saveas(h, sprintf('results/centralizado_ARI_K%d.png',K));
+    saveas(h, sprintf('results/centralizado_VM_K%d.png',K));
 end
 
 for K=[50, 70, 90]
