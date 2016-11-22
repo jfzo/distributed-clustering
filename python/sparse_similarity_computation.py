d84be93f45b287dcc177e1c63274d9e1ec78b6a8
Juan Zamora O <juanzox@gmail.com>
2016-11-22 09:59:12 -0300

    Update on sparse simmilarity computation script

--- python/sparse_similarity_computation.py
+++ python/sparse_similarity_computation.py
@@ -19,7 +19,7 @@ class InstanceFeature(object):
     def __init__(self, f_idx, f_val):
         self.f_idx = f_idx
         self.f_val = f_val
-        
+
     def __eq__(self, other):
         if isinstance(other, InstanceFeature):
             return self.f_val == other.f_val
@@ -29,40 +29,26 @@ class InstanceFeature(object):
         if isinstance(other, InstanceFeature):
             return self.f_val > other.f_val # inverted in order to allow a max-heap
         return NotImplemented
-        
+
     def __gt__(self, other):
         if isinstance(other, InstanceFeature):
             return self.f_val < other.f_val # inverted in order to allow a max-heap
         return NotImplemented
-    
+
     def __str__(self):
         return "IX:{0} -> VAL:{1:.4f}\n".format(self.f_idx, self.f_val)
 
 
 
-
-import sys
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-o","--outfile", type=str, help="Path to the output file where the net will be stored")
-parser.add_argument("-f","--format", type=str, help="Format of the data. Available options: cluto|mat")
-parser.add_argument("-i","--inputfile", type=str, help="Path to the input file in Cluto's format where the sparse vectors are stored")
-args = parser.parse_args()
-
-if not args.outfile or not args.inputfile or not args.format:
-    print "Usage: ./sparse_similarity_computation.py -f cluto|mat -i <path to the input file> -o <path to output file>"
-    sys.exit()
-
-outputfile = args.outfile
-inputfile = args.inputfile
-
-
-# for Cluto's format
-if args.format == "cluto":
-    print "Reading", inputfile,"..."
+def sparse_similarity_from_cluto_file(inputfile):
+    '''
+    sparse_similarity_from_cluto_file(inputfile)
+    :param inputfile: Path to the input file in cluto's sparse format
+    :return: A symmetric instance DOT similarity matrix
+    '''
+    import numpy as np
     in_fm = open(inputfile)
-    N,D,_ = map(int, in_fm.readline().strip().split())
+    N,D,_ = map(int, in_fm.readline().strip().split()) #Number of instances, Number of dimensions and NNZ
 
     idx = range(D)
     for i in range(D):
@@ -81,22 +67,29 @@ if args.format == "cluto":
 
     in_fm.seek(0) # reversing file manager
     in_fm.readline() # bypassing header
-    import numpy as np
 
     S = np.zeros((N,N))
     doc_ix = 1
     for L in in_fm:
         data_L = L.strip().split()
+        S[doc_ix-1, doc_ix-1] = 1.0
         for i in range(0,len(data_L),2):
             f_ix = int(data_L[i])
             # check that feature in the index
             for Dc in idx[f_ix - 1]:
                 if Dc > doc_ix: # symmetric matrix, hence only upper diagonal values matter.
                     S[doc_ix - 1, Dc - 1] += idx[f_ix - 1][doc_ix] * idx[f_ix - 1][Dc]
+                    S[Dc-1, doc_ix-1] = S[doc_ix - 1, Dc - 1]
         doc_ix += 1
     in_fm.close()
+    return S
 
-elif args.format == "mat":
+def sparse_similarity_from_mat_file(inputfile):
+    '''
+    sparse_similarity_from_mat_file(inputfile)
+    :param inputfile: Path to the input file in Matlab's format
+    :return: A symmetric instance DOT similarity matrix
+    '''
     import numpy as np
     import scipy.io as sio
     M = sio.loadmat(inputfile)
@@ -125,6 +118,7 @@ elif args.format == "mat":
 
     S = np.zeros((N,N))
     for doc_ix in range(N):
+        S[doc_ix, doc_ix] = 1.0
         for feat_n in range(D):
             f_val = M[doc_ix,feat_n]
             if f_val != 0:
@@ -132,17 +126,37 @@ elif args.format == "mat":
                 for Dc in idx[feat_n]:
                     if Dc > doc_ix:
                         S[doc_ix, Dc] += idx[feat_n][doc_ix] * idx[feat_n][Dc]
+                        S[Dc, doc_ix] = S[doc_ix, Dc]
 
+    return S
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o","--outfile", type=str, help="Path to the output file where the net will be stored")
+parser.add_argument("-f","--format", type=str, help="Format of the data. Available options: cluto|mat")
+parser.add_argument("-i","--inputfile", type=str, help="Path to the input file in Cluto's format where the sparse vectors are stored")
+args = parser.parse_args()
+
+if not args.outfile or not args.inputfile or not args.format:
+    print "Usage: ./sparse_similarity_computation.py -f cluto|mat -i <path to the input file> -o <path to output file>"
+    sys.exit()
+
+outputfile = args.outfile
+inputfile = args.inputfile
+
+
+# for Cluto's format
+if args.format == "cluto":
+    print "Reading", inputfile,"..."
+    S = sparse_similarity_from_cluto_file(inputfile)
+elif args.format == "mat":
+    S = sparse_similarity_from_mat_file(inputfile)
 else:
     print "Error: Format not supported."
     sys.exit()
 
-# Making the matrix symmetric
-for i in range(S.shape[0] - 1):
-    S[i,i] = 1.0
-    for j in range(i+1, S.shape[1]):
-        S[j,i] = S[i,j]
-S[S.shape[0] - 1, S.shape[0] - 1] = 1.0
 
 # S is a dense symmetric matrix containing the similarity coefficients.
 print "Writing adjacency matrix to CSV file",outputfile
