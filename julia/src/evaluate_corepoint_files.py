d6a2faa302968dcef790dea3530cd4fa1d614ded
JZ <juanzox@gmail.com>
2017-12-03 22:32:44 +0000

    Experimentation scripts added

---
+++ julia/src/evaluate_corepoint_files.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+import argparse
+import sys
+
+from tabulate import tabulate
+
+def print_performance(real_labels, estimated_labels):
+    print("Homogeneity: %0.3f" % metrics.homogeneity_score(real_labels, estimated_labels))
+    print("Completeness: %0.3f" % metrics.completeness_score(real_labels, estimated_labels))
+    print("V-measure: %0.3f" % metrics.v_measure_score(real_labels, estimated_labels))
+    print("Adjusted Rand Index: %0.3f"
+          % metrics.adjusted_rand_score(real_labels, estimated_labels))
+    print("Adjusted Mutual Information: %0.3f"
+          % metrics.adjusted_mutual_info_score(real_labels, estimated_labels))
+
+def obtain_performance(real_labels, estimated_labels):
+    # Homogeneity Completeness  V-measure  Adjusted Rand Index  Adjusted Mutual Information
+    return {
+        "homogeneity":metrics.homogeneity_score(real_labels, estimated_labels),
+        "completeness":metrics.completeness_score(real_labels, estimated_labels),
+        "v-score":metrics.v_measure_score(real_labels, estimated_labels), 
+        "ari":metrics.adjusted_rand_score(real_labels, estimated_labels),
+        "ami":metrics.adjusted_mutual_info_score(real_labels, estimated_labels)
+    }
+
+
+
+parser = argparse.ArgumentParser(description='Sparse data file name.')
+parser.add_argument('-i', type=str, help='file name', required=True)
+parser.add_argument('-f', type=str, help='display format (plain, simple, grid, html, latex, latex_raw, latex_booktabs ...). See tabulate package.', required=True)
+args = parser.parse_args()
+
+DATA_PATH = args.i
+
+print "Evaluating over corepoints generated from data file", DATA_PATH
+
+#cpdata = np.genfromtxt("{0}.corepoints.csv".format(DATA_PATH), delimiter=' ')
+real_lbl = np.loadtxt("{0}.corepoints.labels".format(DATA_PATH),delimiter="\n")
+allreal_lbl = np.loadtxt("{0}.labels".format(DATA_PATH),delimiter="\n")
+
+performances = [];
+headers = ["Method", "V-Score", "ARI", "AMI"]
+
+"""
+db = DBSCAN(eps=0.9, min_samples=10).fit(cpdata)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+#print_performance(real_lbl, labels)
+values = obtain_performance(real_lbl, labels)
+performances.append(["DBSCAN (scikit)",values["v-score"],values["ari"],values["ami"]])
+"""
+labels = np.loadtxt("{0}.dsnnfinal.labels".format(DATA_PATH), delimiter="\n")
+#print_performance(real_lbl, labels)
+values = obtain_performance(allreal_lbl, labels)
+performances.append(["D-SNN (all)",values["v-score"],values["ari"],values["ami"]])
+
+
+labels = np.loadtxt("{0}.corepoints.snn.labels".format(DATA_PATH), delimiter="\n")
+#print_performance(real_lbl, labels)
+values = obtain_performance(real_lbl, labels)
+performances.append(["SNN",values["v-score"],values["ari"],values["ami"]])
+
+labels = np.loadtxt("{0}.corepoints.conncomps.labels".format(DATA_PATH), delimiter="\n")
+#print_performance(real_lbl, labels)
+values = obtain_performance(real_lbl, labels)
+performances.append(["Conn. Comps.",values["v-score"],values["ari"],values["ami"]])
+
+labels = np.loadtxt("{0}.corepoints.cliques.labels".format(DATA_PATH), delimiter="\n")
+#print_performance(real_lbl, labels)
+values = obtain_performance(real_lbl, labels)
+performances.append(["Max Cliques",values["v-score"],values["ari"],values["ami"]])
+
+labels = np.loadtxt("{0}.corepoints.lblprop.labels".format(DATA_PATH), delimiter="\n")
+#print_performance(real_lbl, labels)
+values = obtain_performance(real_lbl, labels)
+performances.append(["Label Prop.",values["v-score"],values["ari"],values["ami"]])
+
+labels = np.loadtxt("{0}.corepoints.dbscan.labels".format(DATA_PATH), delimiter="\n")
+#print_performance(real_lbl, labels)
+values = obtain_performance(real_lbl, labels)
+performances.append(["DBSCAN (julia)",values["v-score"],values["ari"],values["ami"]])
+
+print tabulate(performances, headers, tablefmt=args.f)
---
+++ julia/src/run_experiments.jl
@@ -0,0 +1,189 @@
+using ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--inputfile", "-i"
+            help = "an option with an argument"
+            required = true        
+    end
+
+    return parse_args(s)
+end
+
+
+
+parsed_args = parse_commandline()
+
+DATA_PATH=parsed_args["inputfile"]
+
+
+if length(workers()) > 1
+    println("Removing previous workers...")
+    rmprocs(workers())
+end
+
+
+include("/workspace/distributed_clustering/julia/src/dsnn_IO.jl")
+# Grancoloso
+#addprocs(["158.251.93.5:3308","158.251.93.5:3307","158.251.93.5:3306","158.251.93.5:3305",])
+#addprocs(["158.251.93.5:3308","158.251.93.5:3307","158.251.93.5:3306","158.251.93.5:3305",
+#        "158.251.93.5:3304","158.251.93.5:3303","158.251.93.5:3302","158.251.93.5:3301",])
+
+# Coloso
+#addprocs(["158.251.88.180:3301","158.251.88.180:3302","158.251.88.180:3303","158.251.88.180:3304",])
+
+overall_parameters = DSNN_IO.read_configuration("experiments_config");
+addprocs(overall_parameters["master.nodelist"]);
+
+@everywhere include("/workspace/distributed_clustering/julia/src/dsnn_IO.jl")
+@everywhere include("/workspace/distributed_clustering/julia/src/dsnn_KNN.jl")
+@everywhere include("/workspace/distributed_clustering/julia/src/dsnn_SNN.jl")
+@everywhere include("/workspace/distributed_clustering/julia/src/dsnn_Master.jl")
+
+using Graphs
+using CSV
+
+results = Dict{String,Any}();
+
+DATA_LEN, DATA_DIM = DSNN_IO.get_dimensions_from_input_file(DATA_PATH);
+partitions = DSNN_Master.generate_partitions(length(workers()), DATA_LEN); # N must be extracted from the data.
+
+
+println("\n\n***********************************************************")
+DSNN_Master.start(results, DATA_PATH, partitions, overall_parameters);
+#storing final result
+writedlm(@sprintf("%s.dsnnfinal.labels",DATA_PATH), results["stage2_labels"], "\n");
+
+
+# Experimentation over the obtained corepoints
+D = DSNN_IO.sparseMatFromFile(DATA_PATH, l2normalize=true);
+real_labels = vec(readdlm(@sprintf("%s.labels",DATA_PATH), Int32));
+
+Dw = D[:,results["stage1_corepoints"]];
+cp_real_labels = real_labels[results["stage1_corepoints"]];
+
+CSV.write(@sprintf("%s.corepoints.csv",DATA_PATH), DataFrames.DataFrame(full(transpose(Dw))), delim=' ',  header=false);
+writedlm(@sprintf("%s.corepoints.labels",DATA_PATH), cp_real_labels, "\n");
+
+snnmat, knnmat = DSNN_KNN.get_snnsimilarity(Dw, overall_parameters["corepoint.knn"], l2knng_path=overall_parameters["l2knng.path"]);
+
+adj_mat = snnmat;
+if overall_parameters["corepoint.use_snngraph"]
+    snngraph = DSNN_KNN.get_snngraph(knnmat, snnmat);
+    adj_mat = snngraph;
+end
+
+# Applying SNN-Clustering overthe corepoints
+@time begin
+    println("Applying SNN-Clustering over the corepoints...");
+    cp_results = DSNN_SNN.snn_clustering(overall_parameters["corepoint.eps"], overall_parameters["corepoint.minpts"], adj_mat);
+
+    labels_found = fill(0, size(cp_results["labels"],1));
+    for c in collect(1:size(cp_results["labels"],2))
+        for i in cp_results["labels"][:,c].nzind
+            labels_found[i] = cp_results["clusters"][c]; # extract the right assigned label name
+        end
+    end
+
+    println("Num. Clusters found:",length(unique(labels_found)))
+    if length(find(x->x<0, cp_results["clusters"])) > 0
+        println(@sprintf("Percentage of noise:: %0.2f", 
+                length(cp_results["labels"][:,1].nzind)/size(cp_results["labels"],1)))
+    else
+        println("Amount of noise: 0");
+    end
+    writedlm(@sprintf("%s.corepoints.snn.labels",DATA_PATH), labels_found, "\n");
+end
+
+# Applying Connected components
+@time begin
+    println("Applying Connected Components over the corepoints...");
+    numpoints = size(Dw,2);
+    println("Num. points:",numpoints);
+
+    G = Graphs.simple_adjlist(numpoints, is_directed=false);
+    for i in collect(1:numpoints)
+        for j in adj_mat[:,i].nzind
+            Graphs.add_edge!(G, i, j)
+        end
+    end
+
+    cmps = Graphs.connected_components(G);
+
+    println("Num. connected components:",length(cmps));
+    labels_found = fill(-1, numpoints);
+    for cmp_i in eachindex(cmps)
+        for p in cmps[cmp_i]
+            labels_found[p] = cmp_i;
+        end
+    end
+    println("Num. Clusters found:",length(unique(labels_found)))
+    writedlm(@sprintf("%s.corepoints.conncomps.labels",DATA_PATH), labels_found, "\n");
+
+end
+
+# Applying Maximal Clique to the corepoints
+@time begin
+    println("Applying Maximal Clique over the corepoints...");
+    numpoints = size(Dw,2);
+    println("Num. points:",numpoints);
+
+    G = Graphs.simple_adjlist(numpoints, is_directed=false);
+    for i in collect(1:numpoints)
+        for j in adj_mat[:,i].nzind
+            Graphs.add_edge!(G, i, j)
+        end
+    end
+
+    cmps = Graphs.maximal_cliques(G);
+
+    println("Num. Cliques:",length(cmps));
+    labels_found = fill(-1, numpoints);
+    for cmp_i in eachindex(cmps)
+        for p in cmps[cmp_i]
+            labels_found[p] = cmp_i;
+        end
+    end
+
+    println("Num. Clusters found:",length(unique(labels_found)))
+    writedlm(@sprintf("%s.corepoints.cliques.labels",DATA_PATH), labels_found, "\n");
+
+end
+
+# Applying Label propagation to the corepoints
+@time begin
+    println("Applying Label propagation over the corepoints...")
+    using LightGraphs
+
+    G = LightGraphs.Graph(numpoints)
+    for i in collect(1:numpoints)
+       for j in adj_mat[:, i].nzind
+           if j > i 
+               # maybe a threshold based on adj_mat[j,i] could be used !
+               if ~LightGraphs.add_edge!(G, i, j)
+                   println("[M] Error: Cannot add edge between vertices ",i," and ",j)
+               end
+           end
+       end
+    end
+    labels_found, conv_history = LightGraphs.label_propagation(G);
+    println("Num. Clusters found:",length(unique(labels_found)))
+    writedlm(@sprintf("%s.corepoints.lblprop.labels",DATA_PATH), labels_found, "\n");
+
+end
+
+
+# Applying DBSCAN over the corepoints
+@time begin
+    println("Applying DBSCAN over the corepoints...");
+    using Clustering
+
+    #dbscan_cl = Clustering.dbscan(full(Dw), 0.1, min_neighbors=15);
+    dbscan_cl = Clustering.dbscan(full(1.0 .- adj_mat), overall_parameters["corepoint.dbscan.eps"], overall_parameters["corepoint.dbscan.minpts"]);
+    labels_found = dbscan_cl.assignments;
+    println("Num. Clusters found:",length(unique(labels_found)))
+    writedlm(@sprintf("%s.corepoints.dbscan.labels",DATA_PATH), labels_found, "\n");
+end 
+
